1c30ab9dcb1d987cfec12b2c9c4873ce342627a7
Matteo Lodi <m.lodi@certego.net>
2019-04-18 10:03:18 +0200

    added new observed key

--- conf.py
+++ conf.py
@@ -2,5 +2,5 @@ KNOWN_FILE_TYPES = ["bmp", "avi", "jpeg", "png", "gif", "htm", "ttf"]
 
 KNOWN_FIRST_PATH = ["images", "wpapi", "upx", "rpc", "wpx", "webstore"]
 
-KNOWN_KEYS = ["10291029JSJUYNHG"]
+KNOWN_KEYS = ["10291029JSJUYNHG", "RUnSbNqNTR6CYtV3"]
 
