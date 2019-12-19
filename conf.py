3a48dae4d143607816169d856f50f5a26e3df8b2
Matteo Lodi <m.lodi@certego.net>
2019-12-19 10:48:04 +0100

    added key

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ KNOWN_FILE_TYPES = ["bmp", "avi", "jpeg", "png", "gif", "htm", "ttf"]
 
 KNOWN_FIRST_PATH = ["images", "wpapi", "upx", "rpc", "wpx", "webstore"]
 
-KNOWN_KEYS = ["10291029JSJUYNHG", "10291029JSJUYUON", "10291029JSJUXMPP",
+KNOWN_KEYS = ["10291029JSJUYNHG", "10291029JSJUYUON", "10291029JSJUXMPP", "WIdtM3YCfxhwrbV1",
               "YQiUrgpfMGxlbXo6", "RUnSbNqNTR6CYtV3", "Ht8hzAIwr54JxJ7E", "FI67eGUt2iA80ZOu",
               "M0mhmjJSMZ52hCdl", "yXGynSHJEJBkfAgs", "Htr6bFer4Yc8fbsd", "9JMZX22GaNiClIFh"]
 
