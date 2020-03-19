784cf401ba4b828de65a3d45572373db76f14fcc
Matteo Lodi <m.lodi@certego.net>
2020-03-19 10:31:22 +0100

    added key

--- conf.py
+++ conf.py
@@ -5,5 +5,6 @@ KNOWN_FIRST_PATH = ["images", "wpapi", "upx", "rpc", "wpx", "webstore"]
 KNOWN_KEYS = ["10291029JSJUYNHG", "10291029JSJUYUON", "10291029JSJUXMPP", "Gwe9HMygngWe8kPK",
               "WIdtM3YCfxhwrbV1", "Gu9foUnsY506KSJ1", "32291667LSJUYNUU", "U7yKaYwFde7YtppY",
               "YQiUrgpfMGxlbXo6", "RUnSbNqNTR6CYtV3", "Ht8hzAIwr54JxJ7E", "FI67eGUt2iA80ZOu",
-              "M0mhmjJSMZ52hCdl", "yXGynSHJEJBkfAgs", "Htr6bFer4Yc8fbsd", "9JMZX22GaNiClIFh"]
+              "M0mhmjJSMZ52hCdl", "yXGynSHJEJBkfAgs", "Htr6bFer4Yc8fbsd", "9JMZX22GaNiClIFh",
+              "Xio4U7r3MlO7FwcQ"]
 
