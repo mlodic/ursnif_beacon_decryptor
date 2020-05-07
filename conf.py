caefcf9c5afb9eade48ff3f76e91a328536794fd
Matteo Lodi <m.lodi@certego.net>
2020-05-07 09:55:59 +0200

    added key

--- conf.py
+++ conf.py
@@ -6,5 +6,5 @@ KNOWN_KEYS = ["10291029JSJUYNHG", "10291029JSJUYUON", "10291029JSJUXMPP", "Gwe9H
               "WIdtM3YCfxhwrbV1", "Gu9foUnsY506KSJ1", "32291667LSJUYNUU", "U7yKaYwFde7YtppY",
               "YQiUrgpfMGxlbXo6", "RUnSbNqNTR6CYtV3", "Ht8hzAIwr54JxJ7E", "FI67eGUt2iA80ZOu",
               "M0mhmjJSMZ52hCdl", "yXGynSHJEJBkfAgs", "Htr6bFer4Yc8fbsd", "9JMZX22GaNiClIFh",
-              "Xio4U7r3MlO7FwcQ", "vuARb2EPotEtfAX2", "gh0YeDy3SqQfihUv"]
+              "Xio4U7r3MlO7FwcQ", "vuARb2EPotEtfAX2", "gh0YeDy3SqQfihUv", "W7fx3j0lFvOxT2kF"]
 
