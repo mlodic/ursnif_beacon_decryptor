4cdf38cb294c2cf8fb84e0dee5454eaafe38473f
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2022-02-09 12:01:33 +0100

    added new keys

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
-KNOWN_FILE_TYPES = ["bmp", "avi", "jpeg", "png", "gif", "htm", "ttf"]
+KNOWN_FILE_TYPES = ["bmp", "avi", "jpeg", "png", "gif", "htm", "ttf", "jlk"]
 
-KNOWN_FIRST_PATH = ["images", "wpapi", "upx", "rpc", "wpx", "webstore", "api1", "manifest"]
+KNOWN_FIRST_PATH = ["images", "wpapi", "upx", "rpc", "wpx", "webstore", "api1", "manifest", "drew"]
 
 KNOWN_KEYS = ["10291029JSJUYNHG", "10291029JSJUYUON", "10291029JSJUXMPP", "Gwe9HMygngWe8kPK",
               "WIdtM3YCfxhwrbV1", "Gu9foUnsY506KSJ1", "32291667LSJUYNUU", "U7yKaYwFde7YtppY",
@@ -8,5 +8,6 @@ KNOWN_KEYS = ["10291029JSJUYNHG", "10291029JSJUYUON", "10291029JSJUXMPP", "Gwe9H
               "M0mhmjJSMZ52hCdl", "yXGynSHJEJBkfAgs", "Htr6bFer4Yc8fbsd", "9JMZX22GaNiClIFh",
               "Xio4U7r3MlO7FwcQ", "vuARb2EPotEtfAX2", "gh0YeDy3SqQfihUv", "W7fx3j0lFvOxT2kF",
               "21291029JSJUXMPP", "crk1JovLX6cM9x1e", "1Vjv6r8PLIZClRMo", "OYseV1BdbUtxutxw",
-              "YhhRrXDdbOklWW4W", "30218409ILPAJDUR", "93021029JSJUIPDL", "eel07bCPjiWJLJeU"]
+              "YhhRrXDdbOklWW4W", "30218409ILPAJDUR", "93021029JSJUIPDL", "eel07bCPjiWJLJeU",
+              "56473871MNTYAIDA"]
 
